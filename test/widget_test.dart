06e3350685b647ce70b8e09a3f26671d9c156a74
Bruno Pastre <pastre68@gmail.com>
2024-11-11 20:42:30 +0100

    feat: displays login widget
    When the app opens, we are displaying the login widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:rachadinha/login_widget.dart';
 import 'package:rachadinha/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets(
+    '''
+    GIVEN the user is not authenticated
+    WHEN they open the app
+    THEN the app displays the Apple Sign-In screen
+    AND the user cannot access any other features until signed in.''',
+    (WidgetTester tester) async {
+      // GIVEN
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // WHEN
+      await tester.pumpWidget(const AppLauncher());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+      // THEN
+      expect(find.byType(LoginWidget), findsOne);
+    },
+  );
 }
