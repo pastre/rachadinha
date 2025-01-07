cdd4d957ac73232d5bc0f1c9690eb7d86892640f
bismuto <brunopadovanss@gmail.com>
2025-01-07 18:47:42 -0300

    feat: sets Apple as the OAuth provider in SignInWithAppleAuthenticationProtocolDriver

--- test/unit/lib/sign_in_with_apple_authentication_protocol_driver_test.dart
+++ test/unit/lib/sign_in_with_apple_authentication_protocol_driver_test.dart
@@ -1,30 +1,36 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rachadinha/authentication_protocol_driver.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
-  test('It should sign in with token', () {
+  test('It should sign in with token AND provider should be Apple', () {
     int signInWithIdTokenCallCount = 0;
-
-    signInWithIdTokenStub() {
+    OAuthProvider? actualProvider;
+    spy(provider) {
       signInWithIdTokenCallCount += 1;
+      actualProvider = provider;
     }
 
-    final sut =
-        SignInWithAppleAuthenticationProtocolDriver(signInWithIdTokenStub);
+    final sut = SignInWithAppleAuthenticationProtocolDriver(spy);
+
     sut.signin();
 
     expect(signInWithIdTokenCallCount, equals(1));
+    expect(actualProvider, equals(OAuthProvider.apple));
   });
 }
 
+typedef AuthenticateWithApple = void Function(OAuthProvider);
+
 class SignInWithAppleAuthenticationProtocolDriver
     implements AuthenticationProtocolDriver {
-  void Function() _signInWithIdToken;
+  final AuthenticateWithApple _authenticate;
 
-  SignInWithAppleAuthenticationProtocolDriver(this._signInWithIdToken);
+  SignInWithAppleAuthenticationProtocolDriver(this._authenticate);
+
   @override
   void signin() {
-    _signInWithIdToken();
+    _authenticate(OAuthProvider.apple);
   }
 
   @override
